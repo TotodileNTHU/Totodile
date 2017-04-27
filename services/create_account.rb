93bc090b625db337af9f3b91d37648688a3ecf18
wtlin1228 <wtlin0711@gmail.com>
2017-04-28 03:14:12 +0800

    remove representer for testing

--- services/create_account.rb
+++ services/create_account.rb
@@ -15,13 +15,19 @@ class CreateAccount
 
   register :validate_request_json, lambda { |request_body|
   	begin
-			puts 'request_body ' + request_body
+			# puts 'request_body ' + request_body
       # S_Account is the value object in /values/account.rb
-  	  account_representation = AccountRepresenter.new(S_Account.new)
-  	  puts 'fuck account_representation'
-			puts account_representation.from_json(request_body)
-			Right(account_representation.from_json(request_body))
-  	rescue
+  	  # account_representation = AccountRepresenter.new(S_Account.new)
+  	  # puts 'fuck account_representation'
+			# puts account_representation.from_json(request_body)
+			# Right(account_representation.from_json(request_body))
+  	  data = {
+        uid: request_body['uid'],
+        name: request_body['name'],
+        password: request_body['password']
+      }
+      Right(data)
+    rescue
   	  Left(Error.new('Wrong input account data'))
   	end
   }
