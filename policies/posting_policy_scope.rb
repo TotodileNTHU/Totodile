bf64df6d7e2d1a69c3cfc8385620a4fefb3c3ef3
frank1234211 <frank1234211@gmail.com>
2017-06-06 14:47:12 +0800

    sort resulf of getting all postings

--- policies/posting_policy_scope.rb
+++ policies/posting_policy_scope.rb
@@ -9,9 +9,9 @@ class PostingPolicy
     # default of target_account is nil
     def initialize(current_account,target_account=nil)
       if target_account.nil?
-        @scope = Posting.all
+        @scope =  Posting.all.sort_by{|post| post.created_at}.reverse
       else    #if target_account is not nil, means that current_account want to get postings of target_account
-        @scope = target_account.owned_postings
+        @scope = target_account.owned_postings.sort_by{|post| post.created_at}.reverse
       end
     end
 
